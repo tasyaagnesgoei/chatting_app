7a79f6c65773869c737a3ce6e35dc7c833a63913
unknown <tasyagoei00@gmail.com>
2021-03-21 19:45:11 +0700

    add fonts and images

---
+++ lib/widgets/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0XFF1F8DF5);
+Color whiteColor = Color(0XFFFFFFFF);
+Color lightBlueColor = Color(0XFF9ED0FF);
+Color blackColor = Color(0XFF2C3A59);
+Color greyColor = Color(0XFF808BA2);
+Color greenColor = Color(0XFF29CB9E);
+
+TextStyle titleTextStyle = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle subtitleTextStyle = TextStyle(
+  color: greyColor,
+  fontWeight: FontWeight.w300,
+);
